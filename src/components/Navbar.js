983b0f3af8d5dbfb4f7bf306228866fd5d1b1e3b
Zeeshan Ali Khan <zeealik7@gmail.com>
2021-04-17 23:49:08 +0500

    zeeshan commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="h-12" style={styles.header}>
-      <div className="justify-items-center	h-8 bg-yellow-100 my-2">
+      <div className="justify-items-center h-8 bg-yellow-100 my-2">
         <h1>this is main div</h1>
       </div>
     </div>
